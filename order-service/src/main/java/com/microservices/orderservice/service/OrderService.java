40edbc4eeeac46909ba22f22dac7019227dbacfe
Ghazi Khan <mailme.gkhan@gmail.com>
2023-06-06 23:28:44 +0530

    eureka server implementation completed

--- order-service/src/main/java/com/microservices/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/microservices/orderservice/service/OrderService.java
@@ -20,7 +20,7 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
 
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest) {
 
@@ -33,12 +33,13 @@ public class OrderService {
                 .orderLineItems(orderLineItemsList)
                 .build();
 
+        // service discovery
+
         // call inventory-service, and place order if the product
         // is int stock
-
         List<String> skuCodes = order.getOrderLineItems().stream().map(OrderLineItems::getSkuCode).toList();
-        InventoryResponse[] inventoryResponses = webClient.get()
-                .uri("http://localhost:8082/api/inventory/",
+        InventoryResponse[] inventoryResponses = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory/",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
